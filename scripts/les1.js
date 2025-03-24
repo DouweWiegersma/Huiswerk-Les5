d7ab84034c6ffe9d718830a71185c17a76fd8b40
DouweWiegersma <douwewiegersma17@hotmail.nl>
2025-03-24 12:01:24 +0100

    bijna af

--- scripts/les1.js
+++ scripts/les1.js
@@ -1,6 +1,6 @@
 // opdracht 1
 
-const salesEmployees = "De afdeling Sales heeft" + " " + departments.sales.numberOfEmployees + " " + "medewerkers";
+const salesEmployees = "De afdeling sales heeft" + departments.sales.numberOfEmployees + "aantal medewerkers";
 console.log(salesEmployees);
 
 const descriptionMarketing = " Marketing is een leuke afdeling om te werken." + " " + departments.marketing.description + ".";
@@ -14,23 +14,19 @@ console.log(descriptionVerkoopmanager);
 
 // opdracht 2
 const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]");
-console.log(userInput);
+// console.log(userInput);
 
 const marketing = departments.marketing.description;
 const sales = departments.sales.description;
 const customerService = departments["customer-service"].description;
 
-if (userInput === "marketing"){
+if (userInput === "marketing") {
     console.log("Je koos" + " " + userInput + " " + marketing);
-
-}
-else if (userInput === "sales"){
+} else if (userInput === "sales") {
     console.log("Je koos" + " " + userInput + " " + sales);
-}
-else if (userInput === "customerService"){
+} else if (userInput === "customerService") {
     console.log("Je koos" + " " + userInput + " " + customerService);
-}
-else {
+} else {
     console.error("Ongeldige keuze");
 }
 
@@ -41,6 +37,15 @@ const job2 = departments.marketing.jobs[1].title;
 const job3 = departments.marketing.jobs[2].title;
 const job4 = departments.marketing.jobs[3].title;
 
+const job5 = departments.sales.jobs[0].title;
+const job6 = departments.sales.jobs[1].title;
+const job7 = departments.sales.jobs[2].title;
+const job8 = departments.sales.jobs[3].title;
+
+const description5 = departments.sales.jobs[0].description
+const description6 = departments.sales.jobs[1].description
+const description7 = departments.sales.jobs[2].description
+const description8 = departments.sales.jobs[3].description
 
 const description1 = departments.marketing.jobs[0].description;
 const description2 = departments.marketing.jobs[1].description;
@@ -48,28 +53,28 @@ const description3 = departments.marketing.jobs[2].description;
 const description4 = departments.marketing.jobs[3].description;
 
 
-const userInputs = prompt("je koos marketing. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in." + "\n"
+const choise1 = prompt("je koos " + userInput + ". Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in." + "\n"
     + "\n" + 0 + ": " + job1 + "\n" + 1 + ": " + job2 + "\n" + 2 + ": " + job3 + "\n" + 3 + ": " + job4);
 
 
-switch(userInputs){
+switch (choise1 === "marketing") {
     case "0":
-        console.log("Je koos " + job1 + ". Een uitdagende rol! " + description1 );
+        console.log("Je koos " + job1 + ". Een uitdagende rol! " + description1);
         break;
     case "1":
-        console.log("Je koos " + job2 + ". Een uitdagende rol! " + description2 );
+        console.log("Je koos " + job2 + ". Een uitdagende rol! " + description2);
         break;
     case "2":
-        console.log("Je koos " + job3 + ". Een uitdagende rol! " + description3 );
+        console.log("Je koos " + job3 + ". Een uitdagende rol! " + description3);
         break;
     case "3":
-        console.log("Je koos " + job4 + ". Een uitdagende rol! " + description4 );
+        console.log("Je koos " + job4 + ". Een uitdagende rol! " + description4);
         break;
     default:
         console.log("onjuiste keuze")
 }
 
-//  opdracht 4
+// //  opdracht 4
 
 const answer = {
     afdeeling: userInput,
@@ -88,5 +93,15 @@ const myVars = "employees"
 console.log(obj[myVar] + " is een leuke afdeling om te werken. Er werken op dit moment " + obj[myVars] + " medewerkers.");
 
 
-
-
+document.getElementById('role-title').textContent = 'marketingmanager';
+document.getElementById('department-description').textContent = 'Marketing omvat verschillende strategieën en activiteiten gericht op het promoten van\n' +
+    '        producten, diensten of merken bij het doelpubliek. Het omvat het begrijpen van de behoeften en voorkeuren van\n' +
+    '        consumenten, het uitvoeren van marktonderzoek, het ontwikkelen van marketingplannen en het implementeren van\n' +
+    '        tactieken om potentiële klanten te bereiken en te betrekken. Marketingprofessionals analyseren markttrends,\n' +
+    '        identificeren doelmarkten en creëren overtuigende boodschappen en communicatiestrategieën om merkbekendheid op\n' +
+    '        te bouwen, klantenwerving te stimuleren en klantloyaliteit te bevorderen';
+document.getElementById('role-description').textContent = '>De marketingmanager is verantwoordelijk voor het ontwikkelen en implementeren van\n' +
+    '        marketingstrategieën om de producten of diensten van een bedrijf te promoten. Ze houden toezicht op\n' +
+    '        marktonderzoek, reclamecampagnes en initiatieven voor klantenwerving. Ze analyseren markttrends, identificeren\n' +
+    '        doelgroepen en werken samen met interdisciplinaire teams om marketingdoelen te bereiken en bedrijfsgroei te\n' +
+    '        stimuleren.';
